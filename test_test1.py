ccd8fd5340793348825b781427605f4cbe31eb6c
Fehu4 <areoflo@gmail.com>
2021-05-11 21:25:58 +0200

    Update test_test1.py

--- test_test1.py
+++ test_test1.py
@@ -1,6 +1,18 @@
+from lxml import etree
+
 def inc(x):
     return x + 1
 
+def validate(xml_path: str, xsd_path: str) -> bool:
 
-def test_answer():
-    assert inc(3) == 5
+    xmlschema_doc = etree.parse(xsd_path)
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    xml_doc = etree.parse(xml_path)
+    result = xmlschema.validate(xml_doc)
+
+    return result
+
+
+def test_schema():
+    assert validate("json_file.json","json_schema.schema.json") == true 
