ee4c3f8e7a458fc7e4d783a612376562085dd6c3
Aleksandra Jarzynska <aleksandra.jarzynska.98@gmail.com>
2021-05-20 12:32:04 +0200

    WIP: detecting new files

--- Tests/commit_check.sh
+++ Tests/commit_check.sh
@@ -2,5 +2,5 @@ CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD)
 
 for file in $CHANGED_FILES; do
     echo $file
-    python -c "import test_test1; test_test1.test_schema()"
+    python -c "test_test1.test_schema()"
     done
