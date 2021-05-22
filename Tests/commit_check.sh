3d95a194950d79aa904a7dc19679336a3542772c
arkflo <areoflo@gmail.com>
2021-05-22 17:23:33 +0200

    fix script s

--- Tests/commit_check.sh
+++ Tests/commit_check.sh
@@ -2,6 +2,8 @@ CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD)
 
 for file in $CHANGED_FILES; do
     echo $file
-    if "Model/"  in $file:
+    if "Model/"  in $file
+    then
       python Tests/test_test1.py $file
+    fi
     done
