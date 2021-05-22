0df64c3951e77fa1135170fb1a009f95e75989b0
arkflo <areoflo@gmail.com>
2021-05-22 17:32:08 +0200

    fix script to execute tests only when updating model files

--- Tests/commit_check.sh
+++ Tests/commit_check.sh
@@ -2,7 +2,7 @@ CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD)
 
 for file in $CHANGED_FILES; do
     echo $file
-    if "Model/"  in $file
+    if [[ $file =~ "Model/" ]];
     then
       python Tests/test_test1.py $file
     fi
