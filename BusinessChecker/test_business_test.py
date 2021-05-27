67c656275de28220b3e3c63532a6bec7a3629c7c
TrunX-DA <57214033+TrunX-DA@users.noreply.github.com>
2021-05-27 16:35:20 +0200

    Update test_business_test.py

--- BusinessChecker/test_business_test.py
+++ BusinessChecker/test_business_test.py
@@ -7,6 +7,7 @@ import subprocess
 
 
 #zrobmy jakaś funkcje inicjującą, ktora dla danego pliku_test.py będzie sprawdzać czy zmiany nastąpiły
-#dla plików, któymy _test jest zainteresowany. W szczególności puki co istotne są tylko pliki, które są w folderze Model
+#dla plików, któymy _test jest zainteresowany. W szczególności puki co istotne są tylko pliki, które 
+#są w folderze Model i mają rozszerzenie *.json
 def test_business_test_1():
     assert True == True
