6616ffc50b32b3aab804bf1c4b43aee581d9bc56
Jan Szeszko <jan1024sze@gmail.com>
2021-05-27 14:38:36 +0200

    Create test_business_test.py

---
+++ BusinessChecker/test_business_test.py
@@ -0,0 +1,9 @@
+import sys
+import json
+import jsonschema
+from jsonschema import validate
+from jsonschema import Draft3Validator
+import subprocess
+
+def test_business_test_1():
+    assert True == True
