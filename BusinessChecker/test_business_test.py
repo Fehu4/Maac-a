1a584f02fd7dc0e48fb8ba1da80f2fcd049bbc09
TrunX-DA <57214033+TrunX-DA@users.noreply.github.com>
2021-05-27 16:34:30 +0200

    Update test_business_test.py

--- BusinessChecker/test_business_test.py
+++ BusinessChecker/test_business_test.py
@@ -5,5 +5,8 @@ from jsonschema import validate
 from jsonschema import Draft3Validator
 import subprocess
 
+
+#zrobmy jakaś funkcje inicjującą, ktora dla danego pliku_test.py będzie sprawdzać czy zmiany nastąpiły
+#dla plików, któymy _test jest zainteresowany. W szczególności puki co istotne są tylko pliki, które są w folderze Model
 def test_business_test_1():
     assert True == True
