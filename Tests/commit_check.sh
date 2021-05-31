793e1458534c2dfcf30027faf31d29e460a934b9
Jan Szeszko <jan1024sze@gmail.com>
2021-05-31 17:15:49 +0200

    Delete commit_check.sh

--- Tests/commit_check.sh
+++
@@ -1,9 +0,0 @@
-CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD)
-
-for file in $CHANGED_FILES; do
-    echo $file
-    if [[ $file =~ "Model/" ]];
-    then
-      python Tests/test_test1.py $file
-    fi
-    done
