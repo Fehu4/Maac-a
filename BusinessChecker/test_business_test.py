c9a9c1c88191757c4b17371ce0072d165f4d8f6e
Aleksandra Jarzynska <aleksandra.jarzynska.98@gmail.com>
2021-06-03 09:41:50 +0200

    Business test moved to business checker folder

--- BusinessChecker/test_business_test.py
+++ BusinessChecker/test_business_test.py
@@ -10,4 +10,14 @@ import subprocess
 #dla plików, któymy _test jest zainteresowany. W szczególności puki co istotne są tylko pliki, które 
 #są w folderze Model i mają rozszerzenie *.json
 def test_business_test_1():
-    assert True == True
+
+    correct = True
+
+    files=subprocess.getoutput('git diff-tree --no-commit-id --name-only -r HEAD')
+    for file in files.split(" "):
+        if (file.beginswith('Model/') and file.endswith('.json')):
+            err = check_acronyms("Dictionaries/table_name_acronyms.json",file)
+            if (correct == True and err != ''):
+                correct = False
+    
+    assert correct == True
