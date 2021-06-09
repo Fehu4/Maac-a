0ced694d99c36944e35586c24d04eb77daf7b1ca
Jan Szeszko <jan1024sze@gmail.com>
2021-06-09 20:42:53 +0200

    Update test_test1.py

--- Tests/test_test1.py
+++ Tests/test_test1.py
@@ -46,7 +46,7 @@ def validate_json(json_data, f):
     for error in sorted(errors, key=str):
         #print("Line {} --- ".format(get_error_line(error, json_data)) + error.message)
         #error_text += "Line {}: ".format(get_error_line(error, json_data)) + error.message + "\n"
-        error_text += ''.join([str(elem) for elem in error.path])
+        error_text += ''.join([str(elem)+" " for elem in error.path]) + ": "
         error_text += error.message + "\n"
         errCount += 1
         
